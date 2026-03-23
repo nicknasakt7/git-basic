d6dcce1fb741d66f69b9b689fdaceb51137515db
nicknasakt7 <tntkt7@hotmail.com>
2026-03-23 15:27:02 +0700

    feat(tasks): generate tasks resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { WorkspacesModule } from './workspaces/workspaces.module';
+import { TasksModule } from './tasks/tasks.module';
 import { BoardsModule } from './boards/boards.module';
 import { ColumnsModule } from './columns/columns.module';
 
@@ -12,6 +13,7 @@ import { ColumnsModule } from './columns/columns.module';
     AuthModule,
     UserModule,
     WorkspacesModule,
+    TasksModule,
     BoardsModule,
     ColumnsModule,
   ],
