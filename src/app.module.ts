2469eadaa9f78729311e23b33fb0a809b00902cb
nicknasakt7 <tntkt7@hotmail.com>
2026-03-23 16:46:50 +0700

    Dev (#4)
    * feat(boards): generate boards resource (#1)
    
    * feat(columns): generate columns resource (#2)
    
    * feat(tasks): generate tasks resource (#3)

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,19 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { WorkspacesModule } from './workspaces/workspaces.module';
+import { TasksModule } from './tasks/tasks.module';
+import { BoardsModule } from './boards/boards.module';
+import { ColumnsModule } from './columns/columns.module';
 
 @Module({
-  imports: [AuthModule, UserModule, WorkspacesModule],
+  imports: [
+    AuthModule,
+    UserModule,
+    WorkspacesModule,
+    TasksModule,
+    BoardsModule,
+    ColumnsModule,
+  ],
 
   controllers: [AppController],
   providers: [AppService],
